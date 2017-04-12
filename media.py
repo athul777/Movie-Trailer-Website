b6ae4e72cdf7afbe14a437c43161be351cba8be6
Athul Ganesh <athul777@gmail.com>
2017-04-11 21:22:14 -0700

    Fixed Style Guidelines

--- media.py
+++ media.py
@@ -1,12 +1,17 @@
 '''
-Defines the class Movie so we can create Movie objects (in entertainmen_center.py).
+Defines the class Movie so we can create
+Movie objects (in entertainmen_center.py).
 '''
 
 class Movie():
     """This class provides a way to store movie related information."""
 
-    #The constructor initializes 4 variables: title, storyline, poster image, and Youtube trailer.
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    '''
+    The constructor initializes 4 variables:
+    title, storyline, poster image, and Youtube trailer.
+    '''
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
